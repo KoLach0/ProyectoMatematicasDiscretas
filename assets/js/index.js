216637e20c4c85753221f575ea82ff3db6be5129
Nicolas Albarracin <nicolas.albarracin@dattec.com.co>
2019-11-15 16:45:46 -0500

    ya no se desborda, y mejoro la matriz

--- assets/js/index.js
+++ assets/js/index.js
@@ -18,12 +18,17 @@ $(function () {
 
 function CreateMatriz(params) {
   _MainMatriz = new Array(params); 
-  for (var i = 0; i < params; i++) {
+  var Iscell = true;
+
+  for (var i = 0; i < params; i++) {  
     _MainMatriz[i] = new Array(params);
+    Iscell = (i % 2 ==  0 ? true : false);
     $("#board").append("<div id='row_"+i+"' class='row'></div>");
      for (var j = 0; j < params; j++) {
       _MainMatriz[i][j] = "0";
-      $("#row_"+i+"").append('<div id="'+i+'_'+j+'" class="cell white"></div>');
+      $("#row_"+i+"").append('<div id="'+i+'_'+j+'" class="cell '+(Iscell?"white":"black")+'"></div>');
+      Iscell = (Iscell?false:true);
+
      }
   }
 }
