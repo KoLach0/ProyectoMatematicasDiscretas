b12ee095b49149ab14066c39d7f11775fcb454ee
Nicolas Albarracin <nicolas.albarracin@dattec.com.co>
2019-11-21 15:03:17 -0500

    Se añadio la opcion de cargar la matriz automatica

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,29 +6,49 @@ $(function () {
         showCancelButton: false,
         confirmButtonText: 'Listo',
         showLoaderOnConfirm: true,
-        allowOutsideClick:false,
-        preConfirm: (data) => {
-          CreateMatriz(parseInt(data));
-        },
+        allowOutsideClick:false
       }).then((result) => {
-
+        var sizeMatriz = parseInt(result.value);
+        Swal.fire({
+          title: '¿Desea cargar la matriz de forma automatica?',
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonColor: '#3085d6',
+          cancelButtonColor: '#d33',
+          confirmButtonText: 'Si!',
+          cancelButtonText: 'No'
+        }).then((result) => {
+          if (result.value) {
+            CreateMatriz(sizeMatriz,result.value);
+          }else{
+            CreateMatriz(sizeMatriz,false);
+          }
+        })
       })
 })
 
 
-function CreateMatriz(params) {
-  _MainMatriz = new Array(params); 
-  var Iscell = true;
+$(document).on("keyup",".cell",function () {
+  if (parseInt($(this).val()) != 0 && parseInt($(this).val()) != 1) {
+    $(this).val(""); 
+  }
+})
 
-  for (var i = 0; i < params; i++) {  
-    _MainMatriz[i] = new Array(params);
+function CreateMatriz(size,auto) {
+  _MainMatriz = new Array(size); 
+  var Iscell = true;
+  var valor = 0;
+  for (var i = 0; i < size; i++) {  
+    _MainMatriz[i] = new Array(size);
     Iscell = (i % 2 ==  0 ? true : false);
     $("#board").append("<div id='row_"+i+"' class='row'></div>");
-     for (var j = 0; j < params; j++) {
-      _MainMatriz[i][j] = "0";
-      $("#row_"+i+"").append('<div id="'+i+'_'+j+'" class="cell '+(Iscell?"white":"black")+'"></div>');
+     for (var j = 0; j < size; j++) {
+      valor = Math.round(Math.random() * (1 - 0) + 0);
+      _MainMatriz[i][j] = valor;
+      $("#row_"+i+"").append('<div id="'+i+'_'+j+'" ><input type="number" class="cell '+(Iscell?"white":"black")+'" value="'+(auto?valor:"")+'" '+(auto?"readonly":"")+' ></div>');
       Iscell = (Iscell?false:true);
 
      }
   }
 }
+//https://github.com/jacomyal/sigma.js
